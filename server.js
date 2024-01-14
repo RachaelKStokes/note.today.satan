01e8d7645a911e9708ba073fb2b051d47feb1e9e
unknown <rachael.kate.stokes@gmail.com>
2024-01-14 14:17:46 -0500

    added API routes for get and post. used uuid helper file from a previous activity to generate note id's in this application

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
 const express = require('express');
-
+const fs = require('fs');
 const PORT = 3001;
 const app = express();
+const db = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+const path = require('path');
+
+//Allows use of public folder
+app.use(express.static('public'));
+app.use(express.json());
+
+//API routes
+//Get
+app.get('/api/notes', (req, res) => {
+    fs.readFile(db, (data) => {
+        let dbData = JSON.parse(data);
+        //Returns new database
+        res.json(dbData);
+    });   
+});
+
+//Post
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid;
+    db.push(newNote);
+    fs.writeFileSync(db, JSON.stringify(db));
+    res.json(db);
+});
